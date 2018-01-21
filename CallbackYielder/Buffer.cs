c9d91214d72851b52461dcb9397ca47e1cd9f95e
Joona Luoma <joona.luoma@solteq.com>
2018-01-21 23:45:14 +0200

    Rename files

--- CallbackYielder/Buffer.cs
+++ CallbackYielder/Buffer.cs
@@ -1,22 +1,56 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace CallbackYielder
 {
-    public partial class Buffer<TItem> : IDisposable
+    public partial class Buffer<TItem>
     {
-        public event EventHandler BeforePushingNewItem;
-        public event EventHandler AfterPushingNewItem;
-        public event EventHandler YieldingItem;
-        public event EventHandler StoppingBuffer;
-        public event EventHandler Disposing;
+        private readonly Action<Action<TItem>> _createCallbackMethod;
+        private Action _doFinally;
+        internal List<Action<Buffer<TItem>>> DoOnBufferActions { get; } = new List<Action<Buffer<TItem>>>();
 
-        public void StopBufferingNewItems() => StoppingBuffer?.Invoke(this, null); 
+        public StopIf<TItem> StopIf 
+            => new StopIf<TItem>(this); 
 
-        public void Dispose() => Disposing?.Invoke(this, null);
+        public Func<BlockingCollection<TItem>> InstantiateBufferCollection { get; set; } =
+            () => new BlockingCollection<TItem>();
 
-        internal void InvokeBeforePushingNewItemEvent() => BeforePushingNewItem?.Invoke(this, null);
-        internal void InvokeAfterPushingNewItemEvent() => AfterPushingNewItem?.Invoke(this, null);
+        public Buffer<TItem> Finally(Action doFinally)
+        {
+            _doFinally = doFinally;
+            return this;
+        }
+
+        public Buffer(Action<Action<TItem>> createCallbackMethod)
+        {
+            _createCallbackMethod = createCallbackMethod;
+        }
+        
+        public IEnumerable<TItem> Enumerate()
+        {
+            var collection = InstantiateBufferCollection();
+            this.StoppingBuffer += (e, a) => collection.CompleteAdding();
+            this.Disposing += (e, a) => _doFinally();
+            foreach (var action in DoOnBufferActions)
+                action(this);
+
+            _createCallbackMethod(item =>
+            {
+                this.InvokeBeforePushingNewItemEvent();
+                collection.Add(item);
+                this.InvokeAfterPushingNewItemEvent();
+            });
+            // Now items should be getting pushed to the collection asynchronously
+
+            return collection.GetConsumingEnumerable().Select(i =>
+            {
+                YieldingItem?.Invoke(this, null);
+                return i;
+            });
+        }
     }
 }
--- CallbackYielder/BufferEvents.cs
+++ CallbackYielder/BufferEvents.cs
@@ -1,55 +1,22 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace CallbackYielder
 {
-    public partial class Buffer<TItem>
+    public partial class Buffer<TItem> : IDisposable
     {
-        private readonly Action<Action<TItem>> _createCallbackMethod;
-        private Action _doFinally;
-        internal List<Action<Buffer<TItem>>> DoOnBufferActions { get; } = new List<Action<Buffer<TItem>>>();
+        public event EventHandler BeforePushingNewItem;
+        public event EventHandler AfterPushingNewItem;
+        public event EventHandler YieldingItem;
+        public event EventHandler StoppingBuffer;
+        public event EventHandler Disposing;
 
-        public StopIf<TItem> StopIf 
-            => new StopIf<TItem>(this); 
+        public void StopBufferingNewItems() => StoppingBuffer?.Invoke(this, null); 
 
-        public Func<BlockingCollection<TItem>> InstantiateBufferCollection { get; set; } =
-            () => new BlockingCollection<TItem>();
+        public void Dispose() => Disposing?.Invoke(this, null);
 
-        public Buffer<TItem> Finally(Action doFinally)
-        {
-            _doFinally = doFinally;
-            return this;
-        }
-
-        public Buffer(Action<Action<TItem>> createCallbackMethod)
-        {
-            _createCallbackMethod = createCallbackMethod;
-        }
-        
-        public IEnumerable<TItem> Enumerate()
-        {
-            var collection = InstantiateBufferCollection();
-            this.StoppingBuffer += (e, a) => collection.CompleteAdding();
-            this.Disposing += (e, a) => _doFinally();
-            foreach (var action in DoOnBufferActions)
-                action(this);
-
-            _createCallbackMethod(item =>
-            {
-                this.InvokeBeforePushingNewItemEvent();
-                collection.Add(item);
-                this.InvokeAfterPushingNewItemEvent();
-            });
-            // Now items should be getting pushed to the collection asynchronously
-
-            return collection.GetConsumingEnumerable().Select(i =>
-            {
-                YieldingItem?.Invoke(this, null);
-                return i;
-            });
-        }
+        internal void InvokeBeforePushingNewItemEvent() => BeforePushingNewItem?.Invoke(this, null);
+        internal void InvokeAfterPushingNewItemEvent() => AfterPushingNewItem?.Invoke(this, null);
     }
 }
